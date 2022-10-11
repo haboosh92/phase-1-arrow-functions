3e88006ec45df4a9ade457420ccd0fbeed2cf10d
Hiba <eng.lilil92@gmail.com>
2022-10-12 00:20:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (para1, para2) => {
+    return para1/para2;
+}
+
+const square = (para1) => {
+    return para1*para1;
+}
+
+const add = (para1, para2) => {
+    return para1+para2;
+}
